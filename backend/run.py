d44f6a558bbf770510892d8520b0e6c764b96f4c
Narasimman2676 <narasimman2676@gmail.com>
2026-06-27 16:21:53 +0530

    run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,11 @@
+import os
 from backend.app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
